5372c4450ee4dc89adf19208f80d0d241cfbd8ac
Dan Krazel <dankr2@ac.sce.ac.il>
2022-05-31 10:00:29 +0300

    update for heroku

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,11 @@ app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
 app.use("/uploads", express.static("./uploads"));
 
 
-// // Step 1:
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
-// // Step 2:
-// app.get("*", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+});
 
 export default app;
