5a1b8e48e82c873d912165b26599654b78f71e89
Dan Krazel <dankr2@ac.sce.ac.il>
2022-06-15 15:23:49 +0300

    update

--- backend/api/controllers/files.controller.js
+++ backend/api/controllers/files.controller.js
@@ -3,7 +3,7 @@ import CoursesDAO from "../../dao/coursesDAO.js"
 import fs from "fs"
 import upload from "../../middleware/upload.js"
 import mongodb from "mongodb"
-import {PdfReader} from "pdfreader"
+//import {PdfReader} from "pdfreader"
 
 
 const ObjectId = mongodb.ObjectId
@@ -67,64 +67,64 @@ export default class FilesController {
     //   .join("\n");
     // }
 
-    static async apiGetContentPDF(req, res, next) {
-      let file = req.body.filePath
-      var rows = {}; 
-      var table = new PdfReader.TableParser()
-      const nbCols = 2;
-      const cellPadding = 40; // each cell is padded to fit 40 characters
-      const columnQuantitizer = (item) => parseFloat(item.x) >= 20;
+  //   static async apiGetContentPDF(req, res, next) {
+  //     let file = req.body.filePath
+  //     var rows = {}; 
+  //     var table = new PdfReader.TableParser()
+  //     const nbCols = 2;
+  //     const cellPadding = 40; // each cell is padded to fit 40 characters
+  //     const columnQuantitizer = (item) => parseFloat(item.x) >= 20;
 
-      // function printRows() {
-      //   Object.keys(rows) // => array of y-positions (type: float)
-      //     .sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
-      //     .forEach((y) => console.log((rows[y] || []).join('')));
-      // }
+  //     // function printRows() {
+  //     //   Object.keys(rows) // => array of y-positions (type: float)
+  //     //     .sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
+  //     //     .forEach((y) => console.log((rows[y] || []).join('')));
+  //     // }
 
 
-      const padColumns = (array, nb) =>
-        Array.apply(null, {length: nb}).map((val, i) => array[i] || []);
-  // .. because map() skips undefined elements
+  //     const padColumns = (array, nb) =>
+  //       Array.apply(null, {length: nb}).map((val, i) => array[i] || []);
+  // // .. because map() skips undefined elements
 
-      const mergeCells = (cells) => (cells || [])
-        .map((cell) => cell.text).join('') // merge cells
-        .substr(0, cellPadding).padEnd(cellPadding, ' '); // padding
+  //     const mergeCells = (cells) => (cells || [])
+  //       .map((cell) => cell.text).join('') // merge cells
+  //       .substr(0, cellPadding).padEnd(cellPadding, ' '); // padding
 
-      const renderMatrix = (matrix) => (matrix || [])
-        .map((row, y) => padColumns(row, nbCols)
-        .map(mergeCells)
-        .join(' | ')
-          ).join('\n');
+  //     const renderMatrix = (matrix) => (matrix || [])
+  //       .map((row, y) => padColumns(row, nbCols)
+  //       .map(mergeCells)
+  //       .join(' | ')
+  //         ).join('\n');
 
-          new PdfReader.parseFileItems(file, function(err, item){
-            if (!item ) {
-              // end of file, or page
-              console.log(renderMatrix(table.getMatrix()));
-              res.json({content:renderMatrix(table.getMatrix())})
-            } else if (item.text) {
-              // accumulate text items into rows object, per line
-              table.processItem(item, columnQuantitizer(item));
-            }
-          });  
-        // pdfBuffer contains the file content
-      // new pdfreader.PdfReader().parseFileItems(file, function (err, item) {
-      //     if (err) 
-      //       console.error("error:", err);
-      //     else if (!item){
-      //       printRows();
-      //       console.warn("end of buffer");
-      //       //console.log(arrItems)
-      //       res.json({content: rows})
-      //     }
-      //     else if (item.text){
-      //       //console.log('1',item.text);
-      //       //table.processItem(item, columnQuantitizer(item));
-      //       //processItem(item)
-      //       (rows[item.y] = rows[item.y] || []).push(item.text);
-      //     }
-      //     console.log(item)
-      //   });
-    }
+  //         new PdfReader.parseFileItems(file, function(err, item){
+  //           if (!item ) {
+  //             // end of file, or page
+  //             console.log(renderMatrix(table.getMatrix()));
+  //             res.json({content:renderMatrix(table.getMatrix())})
+  //           } else if (item.text) {
+  //             // accumulate text items into rows object, per line
+  //             table.processItem(item, columnQuantitizer(item));
+  //           }
+  //         });  
+  //       // pdfBuffer contains the file content
+  //     // new pdfreader.PdfReader().parseFileItems(file, function (err, item) {
+  //     //     if (err) 
+  //     //       console.error("error:", err);
+  //     //     else if (!item){
+  //     //       printRows();
+  //     //       console.warn("end of buffer");
+  //     //       //console.log(arrItems)
+  //     //       res.json({content: rows})
+  //     //     }
+  //     //     else if (item.text){
+  //     //       //console.log('1',item.text);
+  //     //       //table.processItem(item, columnQuantitizer(item));
+  //     //       //processItem(item)
+  //     //       (rows[item.y] = rows[item.y] || []).push(item.text);
+  //     //     }
+  //     //     console.log(item)
+  //     //   });
+  //   }
   
 
   
